4b13a5045c6cc86848361ede4b45e07f06433423
Dario Mantegazza <dadomante@gmail.com>
2018-03-01 21:02:07 +0100

    mega update

--- main.py
+++ main.py
@@ -23,6 +23,46 @@ problem = {'rows': data[0, 0],
 print(problem)
 
 
+class Simulation:
+
+    def __init__(self, d):
+        self.rows = d['rows']
+        self.columns = d['columns']
+        self.n_vehicles = d['vehicles']
+        self.n_rides = d['rides']
+        self.bonus = d['bonus']
+        self.steps = d['steps']
+        self.solution = []
+        self.points = 0
+
+        self.time = 0
+        self.rides = [Ride(r, r) for r in range(self.n_rides)]
+        self.cars = [Car(c) for c in range(self.n_vehicles)]
+
+    def print(self):
+        smap = [['.'] * self.columns] * self.rows
+
+        for car in self.cars:
+            x = car.pos[0]
+            y = car.pos[0]
+            smap[x][y] = self.state
+
+        for i in range(self.rows):
+            for j in range(self.columns):
+                print(smap[i][j], end='')
+            print('')
+
+    def run(self,print_map=True):
+        for self.time in range(0, self.steps):
+            for car in self.cars:
+                car.step()
+
+            if print_map:
+                self.print()
+
+        print('Final results', self.points, self.solution, sep='\n-----------\n\t')
+
+
 class Ride:
     def __init__(self, id, row):
         self.done = False,
@@ -57,6 +97,63 @@ class Car:
             else:
                 self.pos[1]-=1# CASO distanza =0 ???
 
-    def lifeCycle(self):
-        ada=1
 
+    def free(self):
+        if self.sim.time == self.rides[0].early_step:
+            self.state = 'o'
+            self.target = self.rides[0].latest_step
+        else:
+            px = self.pos[0]
+            py = self.pos[1]
+            tx = self.target[0]
+            ty = self.target[1]
+
+            if px > tx:
+                self.pos[0] -= 1
+            elif px < tx:
+                self.pos[0] += 1
+            elif py > ty:
+                self.pos[1] -= 1
+            else:
+                self.pos[1] += 1
+
+
+    def lifeCycle(self):
+        px = self.pos[0]
+        py = self.pos[1]
+        tx = self.target[0]
+        ty = self.target[1]
+
+        if px > tx:
+            self.pos[0] -= 1
+        elif px < tx:
+            self.pos[0] += 1
+        elif py > ty:
+            self.pos[1] -= 1
+        elif py < ty:
+            self.pos[1] += 1
+        else: # same place as target
+            if self.state == 'o':
+                self.state = 'f'
+                self.rides.pop()
+
+                self.target = self.rides[0].start_pos
+                if self.pos == self.target:
+                    self.free()
+                else:
+                    tx = self.target[0]
+                    ty = self.target[1]
+
+                    if px > tx:
+                        self.pos[0] -= 1
+                    elif px < tx:
+                        self.pos[0] += 1
+                    elif py > ty:
+                        self.pos[1] -= 1
+                    else: # py < ty:
+                        self.pos[1] += 1
+            else:
+                self.free()
+#
+sim= Simulation()
+sim.run()
